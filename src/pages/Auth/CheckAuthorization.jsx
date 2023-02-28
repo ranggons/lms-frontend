db7bdf5abaadbacdf9cc87b3fe4b48fda835736c
ikhbaaalll <mhmmadikhbal@gmail.com>
2023-02-28 15:10:22 +0700

    chore: create middleware check authorization

---
+++ src/pages/Auth/CheckAuthorization.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CheckAuthorization = (props) => {
+	return <div>CheckAuthorization</div>;
+};
+
+CheckAuthorization.propTypes = {};
+
+export default CheckAuthorization;
