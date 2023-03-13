810ff2094337f607f85f8f0b8bc07a648cd3ab69
Muhammad Ikhbal <mhmmadikhbal@gmail.com>
2023-03-14 00:23:11 +0700

    feat(lms-82): custom base url (#31)

--- src/services/helper.js
+++ src/services/helper.js
@@ -9,14 +9,24 @@ export const getPaginationPage = (count = 1, pageSize) => {
 };
 
 export const baseUrl = () => {
-	const path = window.location.protocol + "//" + window.location.hostname;
+	const path =
+		window.location.protocol +
+		"//" +
+		window.location.hostname +
+		":" +
+		window.location.port;
 
-	if (path.includes("127")) {
+	// ikhbal custom dev
+	if (path.includes("2000")) {
 		return String("http://127.0.0.1:1000/api/v1");
 	}
 
-	if (path.includes("testing.rangon")) {
-		return String(env.API_DEV + env.API_VERSION);
+	if (
+		path.includes("testing.rangon") ||
+		path.includes("127") ||
+		path.includes("localhost")
+	) {
+		return String(env.API_LOCAL + env.API_VERSION);
 	}
 
 	if (path.includes("dev.rangon")) {
